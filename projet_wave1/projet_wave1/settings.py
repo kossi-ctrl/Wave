053b423b46f2485317da3d5c1fca8f742513f32d
Kossi Zangbe <kossi.zangbe@etu.univ-tours.fr>
2026-05-25 10:12:30 +0200

    fix: ajout cloudinary pour les images media

--- projet_wave1/projet_wave1/settings.py
+++ projet_wave1/projet_wave1/settings.py
@@ -39,10 +39,21 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "cloudinary_storage",
     "django.contrib.staticfiles",
     "kobe_wave",
     "rest_framework",
+    "cloudinary",
 ]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
