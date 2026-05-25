18196443a0899069fcb0c3732453653697486fe3
Kossi Zangbe <kossi.zangbe@etu.univ-tours.fr>
2026-05-25 13:07:27 +0200

    fix: url cloudinary sans doublement jpg

--- projet_wave1/kobe_wave/api_views.py
+++ projet_wave1/kobe_wave/api_views.py
@@ -526,7 +526,7 @@ def api_covers(request):
     return Response([
         {
             "id": img.id_image,
-           "url": f"https://res.cloudinary.com/{cloud_name}/image/upload/wave_cover/{img.filename}",
+            "url": f"https://res.cloudinary.com/{cloud_name}/image/upload/wave_cover/{img.filename}.jpg",
             "year": img.year,
             "month": img.month,
             "hex": img.hexadecimal or "#cccccc",
