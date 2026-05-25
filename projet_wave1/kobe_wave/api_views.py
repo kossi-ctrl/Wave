a8214fa84c09173de353f93e0d53ecdea5503e24
Kossi Zangbe <kossi.zangbe@etu.univ-tours.fr>
2026-05-25 11:06:28 +0200

    api_views

--- projet_wave1/kobe_wave/api_views.py
+++ projet_wave1/kobe_wave/api_views.py
@@ -526,7 +526,7 @@ def api_covers(request):
     return Response([
         {
             "id": img.id_image,
-           "url": cloudinary.utils.cloudinary_url(f"wave_cover/{img.filename}")[0],
+           "url": cloudinary.utils.cloudinary_url(f"wave_cover/{img.filename}.jpg")[0],
             "year": img.year,
             "month": img.month,
             "hex": img.hexadecimal or "#cccccc",
