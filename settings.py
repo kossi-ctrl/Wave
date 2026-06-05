836725322a40273294e7d627fe6c2c2d5e6c43c9
Kossi Zangbe <kossi.zangbe@etu.univ-tours.fr>
2026-06-05 19:05:06 +0200

    modifier gitlab

--- settings.py
+++ settings.py
@@ -93,11 +93,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("PGDATABASE", "wave_test"),
-        "USER": os.environ.get("PGUSER", "wave_user"),
-        "PASSWORD": os.environ.get("PGPASSWORD", "wave_pass"),
-        "HOST": os.environ.get("PGHOST", "localhost"),
-        "PORT": os.environ.get("PGPORT", "5432"),
+            "NAME": os.environ.get("PGDATABASE"),
+            "USER": os.environ.get("PGUSER"),
+            "PASSWORD": os.environ.get("PGPASSWORD"),
+            "HOST": os.environ.get("PGHOST"),
+            "PORT": os.environ.get("PGPORT", "5432"),
         }
     }
 CACHES = {
