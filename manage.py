63802479475bcc3f9ba0ddbb0a7acad2204a0a1b
Kossi Zangbe <kossi.zangbe@etu.univ-tours.fr>
2026-06-05 19:59:44 +0200

    Fix DJANGO_SETTINGS_MODULE path

--- manage.py
+++ manage.py
@@ -8,9 +8,9 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-          "projet_wave1.settings"
-          )
+    "DJANGO_SETTINGS_MODULE",
+    "projet_wave1.projet_wave1.settings"
+)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
